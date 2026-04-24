00e63d0b0d153a4c64cc50000199481d8a8c675b
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-24 15:08:29 +0200

    instance variable added to pages controller along with associations checked/added in necessary models for tasks to populate cards on dashboard

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,6 +1,7 @@
 class City < ApplicationRecord
   has_many :profiles
   has_many :checklist_items, dependent: :destroy
+  has_many :tasks
 
   validates :country, presence: true
   validates :name, presence: true
