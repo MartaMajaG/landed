43c8718ac30cc24cdfe63dd87331630eca40629f
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-28 21:29:52 +0200

    final validation change in migration file so nil values dont block migration

--- db/migrate/20260428112617_make_pillar_id_required_on_tasks.rb
+++ db/migrate/20260428112617_make_pillar_id_required_on_tasks.rb
@@ -1,7 +1,11 @@
 class MakePillarIdRequiredOnTasks < ActiveRecord::Migration[8.1]
   def change
-    # Ensure at least one Pillar exists
-    default_pillar = Pillar.first || Pillar.create!(name: "Default Pillar")
+    # Ensure Pillars exist BEFORE running this migration
+    if Pillar.count == 0
+      raise "You must run db:seed before this migration — no Pillars exist."
+    end
+
+    default_pillar = Pillar.first
 
     # Backfill missing pillar_id values BEFORE enforcing NOT NULL
     Task.where(pillar_id: nil).update_all(pillar_id: default_pillar.id)
