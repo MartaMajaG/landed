11099dea39cf2db97e59b722a5ef50a82ed98905
Julian Sommer <sommerjulian@t-online.de>
2026-04-28 13:28:09 +0200

    feat: enforce NOT NULL on tasks.pillar_id and tighten Task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :city
-  belongs_to :pillar, optional: true  # optional until seeds populate all pillar_ids
+  belongs_to :pillar
   has_many :checklist_items, dependent: :destroy
 
   validates :name, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_28_112122) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_28_112617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -268,7 +268,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_28_112122) do
     t.datetime "created_at", null: false
     t.text "description"
     t.string "name"
-    t.bigint "pillar_id"
+    t.bigint "pillar_id", null: false
     t.datetime "updated_at", null: false
     t.string "urgency"
     t.text "why_it_matters"
