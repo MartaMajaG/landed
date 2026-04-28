d2b8e45cf702f315f1ee80083e8f59e9ec0cca77
Stephen McLaren <stephen-mclaren@hotmail.com>
2026-04-28 14:13:12 +0200

    onboarding fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_action :redirect_if_onboardings_incomplete
+  before_action :redirect_if_onboarding_incomplete
 
-  def redirect_if_onboardings_incomplete
+  def redirect_if_onboarding_incomplete
     return unless user_signed_in?
 
     profile = current_user.profile
     if profile.nil?
       current_user.create_profile!
-      return redirect_to onboardings_path
+      return redirect_to onboarding_path
     end
 
     return unless profile.city_id.blank? || profile.arrival_date.blank?
 
-    redirect_to onboardings_path unless request.path == onboardings_path
+    redirect_to onboarding_path unless request.path == onboarding_path
   end
-
-  # def after_sign_out_path_for(resource_or_scope)
-  #   root_path
-  # end
 end
